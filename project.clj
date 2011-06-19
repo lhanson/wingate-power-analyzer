b4f738e5595c4b3ba6f6251d76021b03ccf9c783
Lyle Hanson <lyle2.0@gmail.com>
2011-06-18 19:55:44 -0500

    Reading Excel files and enumerating worksheets

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject wingate-max-power "0.0.1-SNAPSHOT"
-  :description "TODO: add summary of your project"
-  :dependencies [[clojure "1.2.0"]])
+  :description "A program which computes max 1-second power from Wingate test data"
+  :dependencies [[clojure "1.2.0"]
+                 [org.apache.poi/poi-ooxml "3.8-beta3"]])
+
--- src/wingate_max_power/core.clj
+++ src/wingate_max_power/core.clj
@@ -1,1 +1,9 @@
-(ns wingate-max-power.core)
+(ns wingate-max-power.core
+  (:import [java.io FileInputStream])
+  (:import [org.apache.poi.xssf.usermodel XSSFWorkbook]))
+
+(defn- process-file [filename]
+  (let [worksheets (iterator-seq (.iterator (XSSFWorkbook. (FileInputStream. filename))))]
+    (println "Processing file" filename ", has" (count worksheets) "worksheets")))
+
+(map process-file *command-line-args*)
