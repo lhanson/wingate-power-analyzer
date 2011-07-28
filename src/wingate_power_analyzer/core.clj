f2baf44b754a9a911aa974089bb3f48faa2d338f
Lyle Hanson <lyle2.0@gmail.com>
2011-07-28 09:05:34 -0500

    Fixed main method for standalone execution

--- src/wingate_power_analyzer/core.clj
+++ src/wingate_power_analyzer/core.clj
@@ -3,9 +3,5 @@
   (:require [wingate-power-analyzer.ui :as ui]
             [wingate-power-analyzer.processing :as processing]))
 
-(defn -main [& args]
-  (if (apply empty? args)
-    (ui/show-ui)
-    (apply map processing/process-file! args)))
-
-(-main *command-line-args*)
+(defn -main []
+  (ui/show-ui))
